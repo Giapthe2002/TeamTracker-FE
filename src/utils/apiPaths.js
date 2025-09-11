4a6044e6b8fc5f625e19668407fe647696797366
GiapThe2002 <giapthuy2002@gmail.com>
2025-09-11 15:49:51 +0700

    change BASE_URL

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
-
+export const BASE_URL = "https://team-tracker-be.vercel.app/";
+//http://localhost:8000
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
